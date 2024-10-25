3c7ec34f8cf2dd5bcb73a2407e68e41db0bb5e6a
raqueltolomei-ieg <raquel.tolomei@germinare.org.br>
2024-10-25 12:22:08 -0300

    Comentando classe ModelCreator.

--- src/main/java/org/common/ModelCreator.java
+++ src/main/java/org/common/ModelCreator.java
@@ -6,6 +6,13 @@ import java.text.ParseException;
 import java.util.Map;
 
 public class ModelCreator {
+
+    /** Método que cria o objeto Model a partir do nome da tabela e dos parâmetros
+     * @param tabelaNome Nome da tabela
+     * @param params Map com os parâmetros
+     * @return Objeto Model
+     * @throws ParseException Caso ocorra algum erro na criacao do objeto
+     */
     public static Model createModel(String tabelaNome, Map<String, String> params) throws ParseException {
         switch (tabelaNome) {
             case "tag" -> {
