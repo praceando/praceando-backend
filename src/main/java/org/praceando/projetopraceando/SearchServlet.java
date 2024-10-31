83d442e150f4f162e35a416f713463096da7383e
rkhue <85773727+rkhue@users.noreply.github.com>
2024-10-31 07:42:52 -0300

    Esboçando o SearchServlet

--- src/main/java/org/praceando/projetopraceando/SearchServlet.java
+++ src/main/java/org/praceando/projetopraceando/SearchServlet.java
@@ -10,13 +10,25 @@ import org.dao.DAOManager;
 import org.model.Model;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 
 @WebServlet(name="search", value="/search")
 public class SearchServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String tabela = request.getParameter("tabela");
-        String operacao = request.getParameter("operacao");
+        String coluna = request.getParameter("coluna");
+        String filter = request.getParameter("filter");
+        String orderBy = request.getParameter("orderBy");
 
         DAOGeneric<Model> dao = DAOManager.getDAO(tabela);
+
+        assert dao != null;
+        List<Model> models = dao.visualizar();
+
+        Function<Model, Object> comparator;
     }
+
 }
