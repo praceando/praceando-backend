1d46ebbac24bdea230b8fa73e913652e8174e2c5
rkhue <85773727+rkhue@users.noreply.github.com>
2024-11-03 12:53:33 -0300

    Comentando a exceção ConnectionIsNullException

--- src/main/java/org/dao/ConnectionIsNullException.java
+++ src/main/java/org/dao/ConnectionIsNullException.java
@@ -1,5 +1,10 @@
 package org.dao;
 
+/**
+ * Representa a exceção que ocorre quando não é possível se conectar ao
+ * banco de dados. Especificamente quando o DriverManager do JDBC retorna
+ * um objeto Connection null no conectar() da classe Conexao
+ */
 public class ConnectionIsNullException extends RuntimeException {
 
     /**
